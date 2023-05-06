5870a05d89157bb0d44655ff392baf6e5126bacf
negri234279 <rafalejo@live.com>
2023-05-07 00:44:32 +0200

    Remove logo header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -13,11 +13,6 @@ const Header = () => {
             <nav className='bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800'>
                 <div className='flex flex-wrap justify-between items-center mx-auto max-w-screen-xl'>
                     <Link to={'/'} className='flex items-center'>
-                        <img
-                            src='https://flowbite.com/docs/images/logo.svg'
-                            className='mr-3 h-6 sm:h-9'
-                            alt='Flowbite Logo'
-                        />
                         <span className='self-center text-xl font-semibold whitespace-nowrap dark:text-white'>
                             PowerLog
                         </span>
