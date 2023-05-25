edf8f88c28648dba899765c74a1e401d1ca8b713
negri234279 <rafalejo@live.com>
2023-05-26 00:19:44 +0200

    ⚰️ remove dead code

--- src/pages/settings/components/Profile.jsx
+++ src/pages/settings/components/Profile.jsx
@@ -1,10 +1,7 @@
-// import { useAuthContext } from '../../auth/libs/context/auth.context'
 import UserPassword from './UserPassword'
 import UserProfile from './UserProfile'
 
 const Profile = () => {
-    // const { dispatchAuth } = useAuthContext()
-
     return (
         <div className='mx-auto'>
             <h1 className='text-4xl text-center font-bold mb-6'>MI CUENTA</h1>
