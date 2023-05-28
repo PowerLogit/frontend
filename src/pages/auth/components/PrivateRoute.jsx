0a77402fa7f7acd56823b2f8869cd9f0a3c5326d
negri234279 <rafalejo@live.com>
2023-05-28 21:02:20 +0200

    👔 Update routes with roles check

--- src/pages/auth/components/PrivateRoute.jsx
+++ src/pages/auth/components/PrivateRoute.jsx
@@ -1,13 +1,20 @@
 import { Navigate, Outlet, useLocation } from 'react-router-dom'
 import { useAuthContext } from '../libs/context/auth.context'
 
-const PrivateRoute = () => {
-    const { isAuthenticated, token, loading } = useAuthContext()
+const PrivateRoute = ({ roles }) => {
+    const { isAuthenticated, user, token, loading } = useAuthContext()
     const location = useLocation()
 
-    if (token && loading) return <h3>Loading...</h3>
-    else if (isAuthenticated) return <Outlet />
-    else if (loading) return <h3>Loading...</h3>
+    if (token && loading) {
+        return <h3>Loading...</h3>
+    } else if (
+        isAuthenticated &&
+        (!roles || roles.every((role) => user.role.includes(role)))
+    ) {
+        return <Outlet />
+    } else if (loading) {
+        return <h3>Loading...</h3>
+    }
 
     localStorage.setItem('redirectPath', location.pathname)
 
