774bb577a2869bc790f49b34439063b15b85cea5
Negri234279 <rafalejo01@gmail.com>
2023-06-01 12:38:19 +0200

    👔 Add filters in workouts athlete

--- src/pages/workout/libs/constant/filtersAction.js
+++ src/pages/workout/libs/constant/filtersAction.js
@@ -2,6 +2,8 @@ export const FILTERS_ACTION = {
     SET_SORT_BY: 'setSortBy',
     SET_START_DATE: 'setStartDate',
     SET_END_DATE: 'setEndDate',
+    SET_ATHLETE: 'setAthlete',
+    SET_COACH: 'setCoach',
     SET_PAGE: 'setPage',
     SET_ITEM_PER_PAGE: 'setItemPerPage',
     RESET: 'reset',
